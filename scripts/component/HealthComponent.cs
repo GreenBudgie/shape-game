b57683639d484c659d24799676a81ad51e1811bf
GreenBudgie <forestengine@yandex.ru>
2024-02-14 17:34:24 +0300

    Add health component

---
+++ scripts/component/HealthComponent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Component;
+
+public partial class HealthComponent : Node
+{
+
+    [Signal]
+    public delegate void HealthDepletedEventHandler();
+    
+    [Signal]
+    public delegate void HealthChangedEventHandler(double previousHealth, double newHealth);
+
+    [Export] private double _maxHealth;
+
+    private double _health;
+
+    public override void _Ready()
+    {
+        _health = _maxHealth;
+    }
+    
+    public void SetHealth(double newHealth)
+    {
+        var previousHealth = _health;
+        _health = Clamp(newHealth, 0, _maxHealth);
+        EmitSignal(SignalName.HealthChanged, previousHealth, _health);
+        if (_health <= 0)
+        { 
+            HandleHealthDepletion();
+        }
+    }
+
+    public void AddHealth(double healthToAdd)
+    {
+        if (healthToAdd == 0)
+        {
+            return;
+        }
+        SetHealth(_health + healthToAdd);
+    }
+
+    private void HandleHealthDepletion()
+    {
+        EmitSignal(SignalName.HealthDepleted);
+    }
+
+}
--- scripts/enemies/Enemy.cs
+++ scripts/enemies/Enemy.cs
@@ -3,29 +3,6 @@
 public abstract partial class Enemy : CharacterBody2D
 {
 
-    public float Health { get; set; }
 
-    public override void _Ready()
-    {
-        base._Ready();
-        Health = GetMaxHealth();
-    }
-
-    public void Kill()
-    {
-        QueueFree();
-    }
-
-    public void Damage(float damage)
-    {
-        Modulate = new Color(255, 0, 0);
-        Health -= damage;
-        if (Health <= 0)
-        {
-            Kill();
-        }
-    }
-
-    public abstract float GetMaxHealth();
 
 }
