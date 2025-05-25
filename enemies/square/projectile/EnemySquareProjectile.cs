9d4710cac65149e197a9bb89228c4e5a1f73f63a
GreenBudgie <forestengine@yandex.ru>
2025-05-26 01:32:47 +0300

    Add projectile wall hit sound

--- enemies/square/projectile/EnemySquareProjectile.cs
+++ enemies/square/projectile/EnemySquareProjectile.cs
@@ -1,6 +1,8 @@
 ﻿public partial class EnemySquareProjectile : RigidBody2D
 {
 
+    [Export] private AudioStream _hitWallSound = null!;
+    
     public override void _Ready()
     {
         BodyEntered += HandleBodyEntered;
@@ -16,6 +18,22 @@
     
     private void HandleBodyEntered(Node body)
     {
+        if (body.IsInGroup("level_walls"))
+        {
+            var speed = LinearVelocity.Length();
+            if (speed <= 100)
+            {
+                return;
+            }
+
+            var pitch = speed / 1000f + 0.75f;
+            var volume = 1.5f - speed / 1000f;
+            var sound = SoundManager.Instance.PlayPositionalSound(this, _hitWallSound);
+            sound.PitchScale = Clamp(pitch, 0.75f, 1.25f);
+            sound.VolumeLinear = Clamp(volume, 0.2f, 1);
+            return;
+        }
+        
         if (body is not Player)
         {
             return;
