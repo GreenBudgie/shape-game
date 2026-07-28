bf2a48b6aa786f2966dbfbd5768a7c5dd3a5b6a5
GreenBudgie <forestengine@yandex.ru>
2026-07-29 02:22:08 +0300

    Change yin yang path to beam

--- entity/spawnables/projectiles/yin_yang/YinYang.cs
+++ entity/spawnables/projectiles/yin_yang/YinYang.cs
@@ -1,7 +1,8 @@
 public partial class YinYang : Node2D, ISpawnable<YinYang>
 {
     private const float PathRadius = 128f;
-    const float RotationSpeed = 6f;
+    const float RotationSpeedMin = 7f;
+    const float RotationSpeedMax = 9f;
 
     public YinYang Node => this;
 
@@ -9,7 +10,6 @@ public partial class YinYang : Node2D, ISpawnable<YinYang>
     private Node2D _yangFollowTarget = null!;
     private YinYangSphere _yinSphere = null!;
     private YinYangSphere _yangSphere = null!;
-    private Sprite2D _pathSprite = null!;
 
     private float _yinRotation;
     private float _yangRotation = Pi;
@@ -17,8 +17,11 @@ public partial class YinYang : Node2D, ISpawnable<YinYang>
     private SpawnableContext _context = null!;
     private Vector2 _direction;
     private float _speed;
+    private float _rotationSpeed;
 
-    private Tween? _pathAnimationTween;
+    private const float BeamAnimationDuration = 0.15f;
+    private Tween? _beamAnimationTween;
+    private Beam _beam = null!;
 
     private static readonly PackedScene Scene = GD.Load<PackedScene>("uid://cfienje1goost");
 
@@ -33,14 +36,12 @@ public partial class YinYang : Node2D, ISpawnable<YinYang>
         _context = context;
     }
 
-    private Beam _beam = null!;
-
     public override void _Ready()
     {
         _direction = _context.Direction;
         _speed = _context.CalculateStat<SpeedStat>();
+        _rotationSpeed = (float)GD.RandRange(RotationSpeedMin, RotationSpeedMax) * RandomUtils.RandomSign();
 
-        _pathSprite = GetNode<Sprite2D>("Path");
         _yinFollowTarget = GetNode<Node2D>("YinFollowTarget");
         _yangFollowTarget = GetNode<Node2D>("YangFollowTarget");
 
@@ -56,21 +57,21 @@ public partial class YinYang : Node2D, ISpawnable<YinYang>
         SpawnSphere(_yinSphere, _yinFollowTarget);
         SpawnSphere(_yangSphere, _yangFollowTarget);
 
-        _pathSprite.Modulate = Colors.Transparent;
-
-        _pathAnimationTween = CreateTween().SetTrans(Tween.TransitionType.Quad).SetEase(Tween.EaseType.Out);
-        _pathAnimationTween.TweenModulate(_pathSprite, new Color(1, 1, 1, 0.75f), 0.1f);
-
         _beam = Beam.Create()
             .SetFromTo(_yinSphere.GlobalPosition, _yangSphere.GlobalPosition)
             .SetEnergy(2)
-            .SetProgress(1)
+            .SetProgress(0)
             .SetOutlineThickness(5)
-            .SetBeamCount(2)
+            .SetThickness(20)
+            .SetBeamCount(3)
             .SetOutlineColor(ColorScheme.Yellow)
             .SetBeamColor(ColorScheme.Yellow.Lightened(0.5f));
         
         ShapeGame.Instance.AddChild(_beam);
+        
+        _beamAnimationTween = _beam.CreateTween().SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Quad);
+        _beamAnimationTween
+            .TweenProperty(_beam.ShaderMaterial, Beam.ProgressShaderParam, 1, BeamAnimationDuration);
     }
 
     private void SpawnSphere(YinYangSphere sphere, Node2D followTarget)
@@ -89,15 +90,13 @@ public partial class YinYang : Node2D, ISpawnable<YinYang>
 
     public override void _Process(double delta)
     {
-        _yinRotation = (_yinRotation + RotationSpeed * (float)delta) % Tau;
-        _yangRotation = (_yangRotation + RotationSpeed * (float)delta) % Tau;
+        _yinRotation = (_yinRotation + _rotationSpeed * (float)delta) % Tau;
+        _yangRotation = (_yangRotation + _rotationSpeed * (float)delta) % Tau;
 
         _yinFollowTarget.Position = Vector2.FromAngle(_yinRotation) * PathRadius;
         _yangFollowTarget.Position = Vector2.FromAngle(_yangRotation) * PathRadius;
 
         GlobalPosition += (float)delta * _speed * _direction;
-
-        _pathSprite.Rotation -= RotationSpeed * (float)delta / 2f;
 
         if (IsInstanceValid(_yinSphere) && IsInstanceValid(_yangSphere))
         {
@@ -119,13 +118,17 @@ public partial class YinYang : Node2D, ISpawnable<YinYang>
         _yinSphere.Detach();
         _yangSphere.Detach();
         
-        _pathAnimationTween?.Kill();
-        _pathAnimationTween = CreateTween().SetTrans(Tween.TransitionType.Quad).SetEase(Tween.EaseType.Out);
-        _pathAnimationTween.FadeOut(_pathSprite, 0.1f);
-        _pathAnimationTween.Parallel().TweenScale(_pathSprite, 1.3f, 0.1f);
+        _beamAnimationTween?.Kill();
+        _beamAnimationTween = _beam.CreateTween().SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Quad);
+        _beamAnimationTween
+            .TweenProperty(_beam.ShaderMaterial, Beam.ProgressShaderParam, 0, BeamAnimationDuration);
+     
+        _beamAnimationTween.Finished += FullyRemove;
+    }
 
-        _pathAnimationTween.Finished += QueueFree;
-        
+    private void FullyRemove()
+    {
+        QueueFree();
         _beam.QueueFree();
     }
 }
